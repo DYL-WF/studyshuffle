a05dd2fbe4e63b1c626bb22d903b4e08ec5adbb8
Dylan Whiteford <dylan.whiteford@southerncross.solutions>
2024-05-18 20:32:22 +0200

    Initialize project

---
+++ src/main/java/net/sytes/studyshuffle/StudyshuffleApplication.java
@@ -0,0 +1,13 @@
+package net.sytes.studyshuffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyshuffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyshuffleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/sytes/studyshuffle/StudyshuffleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sytes.studyshuffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyshuffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
