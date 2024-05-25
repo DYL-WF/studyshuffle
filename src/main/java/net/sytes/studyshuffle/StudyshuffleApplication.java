220d9d2551c5985d075bf5adb00ad9d8f4417ea6
Dylan Whiteford <dylan.whiteford@southerncross.solutions>
2024-05-25 14:44:18 +0200

    set up secret env

--- src/main/java/net/sytes/studyshuffle/StudyshuffleApplication.java
+++ src/main/java/net/sytes/studyshuffle/StudyshuffleApplication.java
@@ -2,8 +2,15 @@ package net.sytes.studyshuffle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(
+    ignoreResourceNotFound = false,
+    value = {
+        "file:/etc/studyshuffle/application.properties"
+    }
+)
 public class StudyshuffleApplication {
    
 	public static void main(String[] args) {     
